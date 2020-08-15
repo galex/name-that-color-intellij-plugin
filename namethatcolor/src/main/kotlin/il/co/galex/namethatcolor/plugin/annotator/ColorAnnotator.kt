5fca1ec7e2c85abdf61f651eefe9ed34f6d26c73
Alexandre Gherschon <alexandre.gherschon@gmail.com>
2020-08-15 14:09:16 +0300

    FIXES #22 - Update to 2020.2 and removes usage of deprecated IntelliJ API

--- namethatcolor/src/main/kotlin/il/co/galex/namethatcolor/plugin/annotator/ColorAnnotator.kt
+++ namethatcolor/src/main/kotlin/il/co/galex/namethatcolor/plugin/annotator/ColorAnnotator.kt
@@ -2,6 +2,7 @@ package il.co.galex.namethatcolor.plugin.annotator
 
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.xml.XmlTextImpl
 import il.co.galex.namethatcolor.core.manager.ColorNameFinder
@@ -19,11 +20,16 @@ class ColorAnnotator : Annotator {
             if ((element.parent as XmlTextImpl).parentTag?.name == RESOURCES_TAG_NAME) {
 
                 // color list from name that color
-                holder.createErrorAnnotation(element, COLOR_ANNOTATION_MESSAGE)
-                        .registerFix(NameColorIntention(NAME_THAT_COLOR, color, ColorNameFinder::findColor))
+                holder.newAnnotation(HighlightSeverity.ERROR, COLOR_ANNOTATION_MESSAGE)
+                        .newFix(NameColorIntention(NAME_THAT_COLOR, color, ColorNameFinder::findColor))
+                        .registerFix()
+                        .create()
+
                 // material color palette
-                holder.createErrorAnnotation(element, MATERIAL_COLOR_ANNOTATION_MESSAGE)
-                        .registerFix(NameColorIntention(NAME_THAT_MATERIAL_COLOR, color, ColorNameFinder::findMaterialColor))
+                holder.newAnnotation(HighlightSeverity.ERROR, MATERIAL_COLOR_ANNOTATION_MESSAGE)
+                        .newFix(NameColorIntention(NAME_THAT_MATERIAL_COLOR, color, ColorNameFinder::findMaterialColor))
+                        .registerFix()
+                        .create()
             }
         } catch (e: Exception) {
             // found that a color is not valid for us, ignoring it
